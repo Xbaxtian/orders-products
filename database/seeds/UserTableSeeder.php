9160dbabd5a7ed3daab281703902ead20115f8a5
Sebastian <xbaxtian@hotmail.com>
2018-10-21 12:20:48 -0500

    create user and roles models and migrations

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role_admin = Role::where('name', 'admin')->first();
+        $role_vendor = Role::where('name', 'vendor')->first();
+        $role_client = Role::where('name', 'client')->first();
+
+        $user = new User();
+        $user->name = 'Administrator';
+        $user->email = 'admin@email.com';
+        $user->password = bcrypt('123456');
+        $user->role_id = $role_admin->id;
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Vendor';
+        $user->email = 'vendor@email.com';
+        $user->password = bcrypt('123456');
+        $user->role_id = $role_vendor->id;
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Client';
+        $user->email = 'client@email.com';
+        $user->password = bcrypt('123456');
+        $user->role_id = $role_client->id;
+        $user->save();
+    }
+}
