c5b5522a0a255c2bf1b7946c94f1082696a8c02f
Sebastian <xbaxtian@hotmail.com>
2018-10-21 13:56:36 -0500

    products in client view

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Role;
 
 class ClientController extends Controller
 {
@@ -13,6 +14,9 @@ class ClientController extends Controller
 
     public function index()
     {
-        return view('client.home');
+        $vendor_role = Role::where('name','vendor')->first();
+        return view('client.home', [
+            'vendors' => $vendor_role->users
+        ]);
     }
 }
--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -2,21 +2,44 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mb-4">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
                     You are logged in as a Client!
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <table class="table">
+                <thead>
+                    <th>Vendor</th>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Currency</th>
+                    <th>Description</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    @foreach ($vendors as $vendor)
+                        @foreach ($vendor->products as $product)
+                            <tr>
+                                <td>{{$vendor->name}}</td>
+                                <td>{{$product->name}}</td>
+                                <td>{{$product->price}}</td>
+                                <td>{{$product->currency}}</td>
+                                <td>{{$product->description}}</td>
+                                <td><a href="#" class="btn btn-primary">Order</a></td>
+                            </tr>
+                        @endforeach
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
